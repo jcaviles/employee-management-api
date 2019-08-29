4a27d763155d40344c6c12c21c30d22e6aaab1f2
JC <jc_acastellanos@hotmail.com>
2019-08-28 22:14:29 -0500

    added lombok library

--- src/main/java/com/demo/model/Employee.java
+++ src/main/java/com/demo/model/Employee.java
@@ -1,9 +1,11 @@
 package com.demo.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "employee")
+@Data
 public class Employee {
 
 	@Id
@@ -11,47 +13,5 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String emailId;
-
-	public Employee() {
-
-	}
-
-	public Employee(String firstName, String lastName, String emailId) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.emailId = emailId;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmailId() {
-		return emailId;
-	}
-
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
 
 }
